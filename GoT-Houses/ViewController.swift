bf1d8abf38ab21c15acef84978e0be1d431bcedf
Kathryn Tatum <tatumka@bc.edu>
2020-04-05 20:53:08 -0400

    Initial Commit

---
+++ GoT-Houses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoT-Houses
+//
+//  Created by Kathryn Tatum on 4/5/20.
+//  Copyright © 2020 Kathryn Tatum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
